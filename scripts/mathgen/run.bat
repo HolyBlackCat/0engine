8fdc7541b53cd5a6264611307ec386302f96b3e1
HolyBlackCat <blckcat@inbox.ru>
2017-08-10 23:00:06 +0300

    Better mathgen script.

--- scripts/mathgen/run.bat
+++ scripts/mathgen/run.bat
@@ -2,7 +2,6 @@ g++ mathgen.cpp -o mathgen.exe -std=c++17 -Wall -Wextra -pedantic-errors
 pause
 mathgen.exe
 del /F /Q mathgen.exe
-g++ math.h -std=c++17 -Wall -Wextra -pedantic-errors 
-del /F /Q math.h.gch
+g++ math.h -std=c++17 -Wall -Wextra -pedantic-errors -fsyntax-only
 pause
 move /Y math.h ../../src
